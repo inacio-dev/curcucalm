07fefe07f46a05af818742f71f97c39e57bc8f8a
inacio-dev <inaciormgalvao@gmail.com>
2023-09-28 14:53:19 -0300

    fix

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -106,7 +106,7 @@ export default function Order(props: OrderProps) {
         <div className="col-span-1 my-10 flex h-[400px] max-h-[554px] w-full items-center justify-center sm:h-[450px] md:hidden">
           <div className="relative flex h-full w-full flex-col items-center justify-center rounded-lg bg-slate-light-4 shadow-md">
             <Image src={selo} alt="" className="absolute right-4 top-4" />
-            <Image src={selectedImage} loading="lazy" alt="" className="h-auto w-auto" />
+            <Image src={selectedImage} alt="" className="h-auto w-auto" />
 
             <div className="grid grid-cols-2 gap-4 pt-2 text-[14px]">
               <div className="inline-flex items-center gap-1.5">
@@ -204,7 +204,7 @@ export default function Order(props: OrderProps) {
       <div className="col-span-1 hidden h-[450px] max-h-[450px] w-full items-center justify-center md:col-span-5 md:flex">
         <div className="relative flex h-full w-full flex-col items-center justify-center rounded-lg bg-slate-light-4 shadow-md">
           <Image src={selo} alt="" className="absolute right-5 top-5" />
-          <Image src={selectedImage} alt="" className="h-auto w-auto" />
+          {/* <Image src={selectedImage} alt="" loading="lazy" className="h-auto w-auto" /> */}
 
           <div className="grid grid-cols-2 gap-4 pt-2 text-[14px]">
             <div className="inline-flex items-center gap-1.5">
