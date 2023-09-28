b41e3b5580e6964a1ec617db397646029ae06dbc
inacio-dev <inaciormgalvao@gmail.com>
2023-09-28 14:58:31 -0300

    fix

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -204,7 +204,7 @@ export default function Order(props: OrderProps) {
       <div className="col-span-1 hidden h-[450px] max-h-[450px] w-full items-center justify-center md:col-span-5 md:flex">
         <div className="relative flex h-full w-full flex-col items-center justify-center rounded-lg bg-slate-light-4 shadow-md">
           <Image src={selo} alt="" className="absolute right-5 top-5" />
-          {/* <Image src={selectedImage} alt="" loading="lazy" className="h-auto w-auto" /> */}
+          <Image src={selectedImage} alt="" className="h-auto w-auto" />
 
           <div className="grid grid-cols-2 gap-4 pt-2 text-[14px]">
             <div className="inline-flex items-center gap-1.5">
@@ -237,7 +237,6 @@ export default function Order(props: OrderProps) {
                     quantity,
                     coupon: selectedCoupon,
                   })}`}
-                  replace
                   scroll={false}
                   className={clsx(
                     'flex h-[52px] w-full items-center justify-center rounded-lg border-2 px-[46px] text-center font-normal transition-colors duration-300 hover:border-slate-dark-6 hover:font-bold',
